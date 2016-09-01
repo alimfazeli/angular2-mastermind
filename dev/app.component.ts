a0479495767c1501146e32d89c4f6fa96fe39c45
Ali Fazeli <alimfazeli@gmail.com>
2016-09-01 12:01:36 +0430

    final game

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,19 +1,24 @@
 import {Component} from 'angular2/core';
 import {BoardComponent} from './mastermind/board.component';
 import {SetupComponent} from './mastermind/setup.component';
+import {NavigationComponent} from './mastermind/navigation.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'templates/main.html',
-    directives: [BoardComponent,SetupComponent]
+    directives: [BoardComponent,SetupComponent, NavigationComponent]
 })
 
 export class AppComponent {
     gameData = {
-        name: 'Ali'
+        name: 'Niki'
     }
 
     onUpdate(name) {
         this.gameData.name = name;
+    }
+
+    winHandler(barikalla) {
+        alert(barikalla);
     }
 }
--- dev/mastermind/board.component.ts
+++ dev/mastermind/board.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, HostListener} from 'angular2/core';
+import {Component, Input, HostListener, EventEmitter, Output} from 'angular2/core';
 import {OnInit} from 'angular2/core';
 
 @Component({
@@ -11,6 +11,8 @@ export class BoardComponent implements OnInit {
 
     private possibleColors = ['green', 'blue', 'yellow', 'red', 'purple', 'orange'];
 
+    @Output() WinEvent = new EventEmitter();
+
     private displayColors = {
         green: '#66ff99',
         blue: '#0066cc',
@@ -20,8 +22,10 @@ export class BoardComponent implements OnInit {
         orange: '#ff8c1a',
     }
 
-    attempts: any[] =[];
-    
+
+
+    attempts: any[] = [];
+
     answer: string[] = ["0","0","0","0"];
 
     correctAnswer:string[] = [];
@@ -40,7 +44,48 @@ export class BoardComponent implements OnInit {
     submitAnswer() {
         this.attempts.push(this.answer);
 
+        this.checkAnswer();
         this.clearAnswer();
+    }
+
+    checkAnswer() {
+        let checkResult = {
+            correctColors: 0,
+            correctPlace: 0,
+        };
+        let that = this;
+        this.answer.forEach( function(color, index) {
+            if (that.colorExists(color)) {
+                checkResult.correctColors++;
+            }
+            if ( that.placeIsCorrect(color, index)) {
+                checkResult.correctPlace++;
+            }
+        });
+
+        let attemptData = {
+            answer: this.answer,
+            checkResult: ` ${checkResult.correctColors} number of colors exist in answer
+             and ${checkResult.correctPlace} number are in correct place`
+        }
+
+
+        this.attempts.push(attemptData);
+
+        if (checkResult.correctPlace == 4) {
+                this.WinEvent.emit("Barikallaa!!");
+                console.log('tedsdasd');
+        }
+    }
+
+    private colorExists(color) {
+        if( this.correctAnswer.indexOf(color) >= 0) {
+            return true;
+        }
+    }
+
+    private placeIsCorrect(color, index) {
+        return this.correctAnswer[index] == color;
     }
 
     private clearAnswer() {
